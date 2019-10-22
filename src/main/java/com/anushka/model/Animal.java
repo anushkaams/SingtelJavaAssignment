2c9240a2667f2f00c5478acb4dcc4ee0be259e99
Senarathna <asenarathn@wiley.com>
2019-10-22 22:15:48 +0530

    A 1 b - make Animal class abstract and added abstract fly method.

--- src/main/java/com/anushka/model/Animal.java
+++ src/main/java/com/anushka/model/Animal.java
@@ -1,6 +1,13 @@
 package com.anushka.model;
 
-public class Animal {
+/**
+ * Animal model class
+ */
+
+public abstract class Animal {
+
+    public abstract void fly();
+
     public void walk(){
         System.out.println("I am walking");
     }
