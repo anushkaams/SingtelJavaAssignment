84aee05fb0ac7367ebb979c63c78a187fadbbc3b
Senarathna <asenarathn@wiley.com>
2019-10-22 22:03:36 +0530

    Initial comment with exercise files

---
+++ src/main/java/com/anushka/Solution.java
@@ -0,0 +1,12 @@
+package com.anushka;
+
+import com.anushka.model.Bird;
+
+public class Solution {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
